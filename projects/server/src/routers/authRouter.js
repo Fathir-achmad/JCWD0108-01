89fc836d7418cf9bcd03a4560a00f3ff70aca0c7
ZaidHakim11 <132432732+ZaidHakim11@users.noreply.github.com>
2023-07-27 14:07:34 +0700

    coba (#3)

--- projects/server/src/routers/authRouter.js
+++ projects/server/src/routers/authRouter.js
@@ -4,10 +4,10 @@ const { checkUsername, checkEmail, checkPassword, checkUsernameExist, checkEmail
 
 const router = require('express').Router()
 
-router.post('/register', checkUsername, checkEmail, checkPassword, checkUsernameExist, checkEmailExist, authController.register)
+router.post('/add', checkUsername, checkEmail, checkPassword, checkUsernameExist, checkEmailExist, authController.register)
 router.post('/login', checkUsername, checkPassword, authController.login)
-router.get('/keep', verifyToken, verifyAdmin, authController.keeplogin)
-router.put('/forgotpass', checkEmail, authController.forgotPassword)
-router.patch('/resetpass', verifyToken, verifyAdmin, authController.resetPassword)
+router.get('/keep', verifyToken, authController.keeplogin)
+router.put('/forgetpass', checkEmail, authController.forgetPassword)
+router.patch('/resetpass', verifyToken, checkPassword, authController.resetPassword)
 
 module.exports = router
